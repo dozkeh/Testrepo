a3ad545951ef186b3b10704b251475faaa9e940f
dozkeh <romina.kehl@me.com>
2021-01-16 23:56:18 +0100

    Ready to overwork components

--- testproject/src/app/mymodule/mymodule-routing.module.ts
+++ testproject/src/app/mymodule/mymodule-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeatureRominaComponent } from './feature-romina/feature-romina.component';
 import { FeatureMerlinComponent } from './feature-merlin/feature-merlin.component';
 import { FeatureHenningComponent } from './feature-henning/feature-henning.component';
+import { FeatureJanComponent } from './feature-jan/feature-jan.component';
 
 const routes: Routes =[
   { path: 'henning', component: FeatureHenningComponent },
-  { path: '**', redirectTo: 'henning' }
+  { path: 'merlin', component: FeatureMerlinComponent },
+  { path: 'jan', component: FeatureJanComponent }
 ]
 
 @NgModule({
