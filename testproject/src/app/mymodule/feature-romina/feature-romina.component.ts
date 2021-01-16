5fdb490eb237be660f7bb263b9170b27e61e7aa8
dozkeh <romina.kehl@me.com>
2021-01-16 17:42:19 +0100

    Task #09 feature 1 init

---
+++ testproject/src/app/mymodule/feature-romina/feature-romina.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-romina',
+  templateUrl: './feature-romina.component.html',
+  styleUrls: ['./feature-romina.component.scss']
+})
+export class FeatureRominaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
